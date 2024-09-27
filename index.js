396de86f829012d1d6e069c542feaf1f1a22f6cf
Sakutsadragon <22CS01033@iitbbs.ac.in>
2024-09-27 21:56:56 +0530

    updatedd

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require("express");
-const cors= require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoutes=require("./routes/userRoutes")
-const adminRoutes=require("./routes/adminRoutes")
-const app= express();
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const app = express();
 require("dotenv").config();
+const corsOptions = {
+    origin: "https://ticticket-frontend.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", 
+    credentials: true, 
+    optionsSuccessStatus: 204 ,
+};
 
-
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use("/api/auth",userRoutes)
-app.use("/api/autha",adminRoutes)
+app.use("/api/auth", userRoutes);
+app.use("/api/autha", adminRoutes);
 
 mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log("DB connected Successfully!!!");
-}).catch((err)=>{
-    console.log(err.message);
-});
+    .then(() => {
+        console.log("DB connected Successfully!!!");
+    }).catch((err) => {
+        console.log(err.message);
+    });
 
-const server = app.listen(process.env.PORT,()=>{
-    console.log(`Server is connected !!! ${process.env.PORT}`)
-})
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is connected !!! ${process.env.PORT}`);
+});
