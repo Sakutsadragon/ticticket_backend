00dac032b5badf979b788802f0616eca6b84eb1e
Sakutsadragon <22CS01033@iitbbs.ac.in>
2024-09-27 22:21:00 +0530

    added brces in cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const corsOptions = {
 };
 
 // app.use(cors(corsOptions));
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", userRoutes);
 app.use("/api/autha", adminRoutes);
